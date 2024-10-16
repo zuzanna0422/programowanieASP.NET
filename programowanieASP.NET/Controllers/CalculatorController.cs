64189c772e8c0ceab8390101e958960bf57faa92
Zuzanna Cholewa <zuzanna.cholewa@gmail.com>
2024-10-16 11:21:09 +0200

    calculator model

---
+++ programowanieASP.NET/Controllers/CalculatorController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using programowanieASP.NET.Models;
+
+namespace programowanieASP.NET.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Result(Calculator model)
+        {
+            if (!model.IsValid())
+            {
+                return View("Error");
+            }
+            return View(model);
+        }
+        public IActionResult Form()
+        {
+            return View();
+        }
+    }
+}
---
+++ programowanieASP.NET/Models/Calculator.cs
@@ -0,0 +1,52 @@
+﻿using programowanieASP.NET.Controllers;
+
+namespace programowanieASP.NET.Models
+{
+    public class Calculator
+    {
+        public Operator? Operator { get; set; }
+        public double? X { get; set; }
+        public double? Y { get; set; }
+
+        public String Op
+        {
+            get
+            {
+                switch (Operator)
+                {
+                    case Controllers.Operator.Add:
+                        return "+";
+                    case Controllers.Operator.Div:
+                        return "/";
+                    case Controllers.Operator.Sub:
+                        return "-";
+                    case Controllers.Operator.Mul:
+                        return "*";
+                default:
+                        return "";
+                }
+            }
+        }
+
+        public bool IsValid()
+        {
+            return Operator != null && X != null && Y != null;
+        }
+
+        public double Calculate()
+        {
+            switch (Operator)
+            {
+                case Controllers.Operator.Add:
+                    return (double)(X + Y);
+                case Controllers.Operator.Sub:
+                    return (double)(X - Y);
+                case Controllers.Operator.Mul:
+                    return (double)(X * Y);
+                case Controllers.Operator.Div:
+                    return (double)(X / Y);
+            default: return double.NaN;
+            }
+        }
+    }
+}
