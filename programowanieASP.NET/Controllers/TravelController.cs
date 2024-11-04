0749bd2416694235a703b19800a4070ce3392fbb
Zuzanna Cholewa <zuzanna.cholewa@gmail.com>
2024-11-04 14:12:45 +0100

    poprawiony kod

---
+++ programowanieASP.NET/Controllers/TravelController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using programowanieASP.NET.Models;
+
+namespace programowanieASP.NET.Controllers
+{
+    public class TravelController : Controller
+    {
+        static Dictionary<int, TravelModel> _travel = new Dictionary<int, TravelModel>();
+
+        public IActionResult Index()
+        {
+            return View(_travel);
+        }
+
+        [HttpGet]
+        public IActionResult TravelForm()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(TravelModel travel)
+        {
+            if (ModelState.IsValid)
+            {
+                int id = _travel.Keys.Count != 0 ? _travel.Keys.Max() : 0;
+                travel.Id = id + 1;
+                _travel.Add(travel.Id, travel);
+
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                return View(travel);
+            }
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (_travel.ContainsKey(id))
+                return View(_travel[id]);
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TravelModel travel)
+        {
+            if (ModelState.IsValid)
+            {
+                _travel[travel.Id] = travel;
+                return RedirectToAction("Index");
+            }
+            return View(travel);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            if (_travel.ContainsKey(id))
+                return View(_travel[id]);
+            return NotFound();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (_travel.ContainsKey(id))
+                return View(_travel[id]);
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            if (_travel.ContainsKey(id))
+                _travel.Remove(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ programowanieASP.NET/Models/TravelModel.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using programowanieASP.NET.Controllers;
+
+namespace programowanieASP.NET.Models
+{
+    public class TravelModel
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać nazwę!")]
+        public string Nazwa { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Proszę podać date rozpoczęcia!")]
+        public DateTime DataRozpoczecia { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Proszę podać date zakończenia!")]
+        public DateTime DataZakonczenia { get; set; }
+
+        [MinLength(length: 3, ErrorMessage = "Miejsce początkowe musi mieć co najmniej 3 litery!")]
+        [Required(ErrorMessage = "Proszę podać miejsce początkowe!")]
+        public string MiejscePoczatkowe { get; set; }
+
+        [MinLength(length: 3, ErrorMessage = "Miejsce końcowe musi miec co najmniej 3 litery!")]
+        [Required(ErrorMessage = "Proszę podać nazwę końcową!")]
+        public string MiejsceKoncowe { get; set; }
+
+        [Required(ErrorMessage = "Uczestnicy sa wymagani")]
+        public List<string> Uczestnicy { get; set; }
+
+        [Required(ErrorMessage = "Przewodnik jest wymagany")]
+        public string Przewodnik { get; set; }
+
+    }
+}
+
