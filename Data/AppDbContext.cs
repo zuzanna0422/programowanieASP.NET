cee9df93c5bde987a975d9ba6501304cb64e7247
Zuzanna Cholewa <zuzanna.cholewa@gmail.com>
2024-11-18 15:03:08 +0100

    migration and update of database

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,49 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+
+namespace Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TravelEntity> Travel { get; set; } 
+        private string DbPath { get; set; }
+        public AppDbContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "contacts.db");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+        options.UseSqlite($"Data Source={DbPath}");
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TravelEntity>().HasData(
+                new TravelEntity()
+                {
+                    Id = 1,
+                    Nazwa = "wycieczka ",
+                    DataRozpoczecia = new DateTime(2024, 12, 5),
+                    DataZakonczenia = new DateTime(2024, 12, 10),
+                    MiejscePoczatkowe = "Krakow",
+                    MiejsceKoncowe = "Łódź",
+                    Uczestnicy = ["Ola", "Ala"],
+                    Przewodnik = "Alek",
+                },
+                new TravelEntity()
+                {
+                     Id = 2,
+                     Nazwa = "wycieczka ",
+                     DataRozpoczecia = new DateTime(2024, 12, 5),
+                     DataZakonczenia = new DateTime(2024, 12, 10),
+                     MiejscePoczatkowe = "Krakow",
+                     MiejsceKoncowe = "Łódź",
+                     Uczestnicy = ["Ola", "Ala"],
+                     Przewodnik = "Alek",
+                }
+            );
+        }
+
+    }
+}
---
+++ Data/Migrations/20241118140007_InitialCreate.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Travel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Priority = table.Column<int>(type: "INTEGER", nullable: false),
+                    Nazwa = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    DataRozpoczecia = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataZakonczenia = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    MiejscePoczatkowe = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    MiejsceKoncowe = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Uczestnicy = table.Column<string>(type: "TEXT", nullable: false),
+                    Przewodnik = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Travel", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Travel",
+                columns: new[] { "Id", "DataRozpoczecia", "DataZakonczenia", "MiejsceKoncowe", "MiejscePoczatkowe", "Nazwa", "Priority", "Przewodnik", "Uczestnicy" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 12, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 12, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "Łódź", "Krakow", "wycieczka ", 0, "Alek", "[\"Ola\",\"Ala\"]" },
+                    { 2, new DateTime(2024, 12, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 12, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "Łódź", "Krakow", "wycieczka ", 0, "Alek", "[\"Ola\",\"Ala\"]" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Travel");
+        }
+    }
+}
