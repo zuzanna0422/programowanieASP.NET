4bbd40f7d39166e8b5f3d59e1dbd1e1f22b65356
Zuzanna Cholewa <zuzanna.cholewa@gmail.com>
2024-11-19 13:15:51 +0100

    service added

--- programowanieASP.NET/Models/ITravelService.cs
+++ programowanieASP.NET/Models/ITravelService.cs
@@ -1,4 +1,9 @@
 ﻿
+using Data.Entities;
+using Data;
+using programowanieASP.NET.Mappers;
+using System.Diagnostics;
+
 namespace programowanieASP.NET.Models
 {
     public interface ITravelService
@@ -8,5 +13,40 @@ namespace programowanieASP.NET.Models
         void Update(TravelModel travel);
         List<TravelModel> FindAll();
         TravelModel? FindById(int travel);
+    }
+
+    public class EFTravelService : ITravelService
+    {
+        private AppDbContext _context;
+        public EFTravelService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public int Add(TravelModel travel)
+        {
+            var e = _context.Travel.Add(TravelMapper.ToEntity(travel));
+            _context.SaveChanges();
+            return e.Entity.Id;
+        }
+        public void Delete(int id)
+        {
+            TravelEntity? find = _context.Travel.Find(id);
+            if (find != null)
+            {
+                _context.Travel.Remove(find);
+            }
+        }
+        public List<TravelModel> FindAll()
+        {
+            return _context.Travel.Select(e => TravelMapper.FromEntity(e)).ToList(); ;
+        }
+        public TravelModel? FindById(int id)
+        {
+            return TravelMapper.FromEntity(_context.Travel.Find(id));
+        }
+        public void Update(TravelModel travel)
+        {
+            _context.Travel.Update(TravelMapper.ToEntity(travel));
+        }
     }
 }
