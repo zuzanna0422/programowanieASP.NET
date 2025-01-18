1de4cb2db1af3798a70fa6464e427324cc6bac5f
Zuzanna Cholewa <zuzanna.cholewa@gmail.com>
2025-01-18 22:14:53 +0100

    publisher viewmodel added

---
+++ WebApplicationVideoGames/Models/ViewModels/ViewModel.cs
@@ -0,0 +1,30 @@
+namespace WebApplicationVideoGames.Models.ViewModels;
+
+public class ViewModel
+{
+    public class PublisherDetailsViewModel
+    {
+        public string PublisherName { get; set; }
+        public List<GameDetails> Games { get; set; }
+    }
+
+    public class GameDetails
+    {
+        public string GameName { get; set; }
+        public List<PlatformDetails> Platforms { get; set; }
+    }
+
+    public class PlatformDetails
+    {
+        public string PlatformName { get; set; }
+        public int? ReleaseYear { get; set; }
+        public List<RegionSalesDetails> SalesByRegion { get; set; }
+    }
+
+    public class RegionSalesDetails
+    {
+        public string RegionName { get; set; }
+        public double? NumSales { get; set; }
+    }
+
+}
